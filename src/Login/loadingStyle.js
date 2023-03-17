99d9ad81cdde930af7ba086245e51fc20c39d1cd
aparnaaravindp <114520875+aparnaaravindp@users.noreply.github.com>
2023-03-16 22:20:51 -0300

    Kenzie Hub Complete

--- src/Login/loadingStyle.js
+++ src/Login/loadingStyle.js
@@ -6,28 +6,11 @@ export const StyledLoadingDiv = styled.div`
   min-height: 100vh;
   margin: 0 auto;
   padding: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   img {
-    margin-top: 100px;
-  }
-
-  @media (min-width: 400px) {
-    img {
-      margin-top: 120px;
-      margin-left: 100px;
-    }
-  }
-
-  @media (min-width: 700px) {
-    img {
-      margin-top: 120px;
-      margin-left: 260px;
-    }
-  }
-
-  @media (min-width: 900px) {
-    img {
-      margin-top: 150px;
-      margin-left: 500px;
-    }
+    max-width: 120px;
+    width: 100%;
   }
 `;
